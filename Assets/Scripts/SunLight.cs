bebc25f8712f337453bb7b075c294e9d43fa115d
ChaoTheChild <cfu@c.ringling.edu>
2019-02-21 11:09:34 -0500

    some more art

---
+++ Assets/Scripts/SunLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunLight : MonoBehaviour
+{   
+    int rotateX;
+    void Start()
+    {   
+        rotateX = 0;
+        transform.eulerAngles = new Vector3(rotateX,-30,0);
+        InvokeRepeating("SunRotate",0,5);
+    }
+
+    // Update is called once per frame
+
+    void SunRotate(){
+        if(rotateX < 180) {
+           rotateX += 1;
+           transform.eulerAngles = new Vector3(rotateX,-30,0);
+       }else{
+           rotateX = -180;
+       }
+    }
+}
